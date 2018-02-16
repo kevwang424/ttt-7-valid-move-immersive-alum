672243bc83b2f8a35f788d96971f567e3969fa83
Kevin Wang <kev.wang424@gmail.com>
2018-02-16 17:58:44 -0500

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 # code your #valid_move? method here
-
+def valid_move?(board, i)
+  if i < board.length && board[i].strip.empty?
+    return true
+  else
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
